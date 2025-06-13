757805808f2f0524dc53bca73836e64a5dc56af9
root <e.leyian@alustudent.com>
2025-06-13 05:48:16 -0700

    submission_reminder_erin-leyian

---
+++ root/submission_reminder_erin-leyian/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+app_dir="/root"
+
+chmod +x "/root/submission_reminder_erin-leyian/app/reminder.sh"
+chmod +x "/root/submission_reminder_erin-leyian/modules/functions.sh"
+chmod +x "/root/submission_reminder_erin-leyian/startup.sh"
+
+"/root/submission_reminder_erin-leyian/app/reminder.sh"
